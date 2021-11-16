02d5bae480faa8e97accb6974b88230863d9ffd0
Rohit joshi <rohitjoshi9july@gmail.com>
2021-11-16 12:36:50 +0530

    cors update

--- src/main/java/com/tcs/angular/creditcard/util/WebSecurityConfig.java
+++ src/main/java/com/tcs/angular/creditcard/util/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
-		httpSecurity.cors().and().csrf().disable()
+		httpSecurity.csrf().disable().cors().and()
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/authenticate", "/register","/check").permitAll().
 				// all other requests need to be authenticated
